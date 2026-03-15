df0d157d0e1ea9c8f2bb8863adf6231252ccaf4e
Michał Górny <mgorny@gentoo.org>
2026-03-15 19:21:49 +0100

    Fix UnboundLocalError
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- assign-pull-requests-codeberg.py
+++ assign-pull-requests-codeberg.py
@@ -256,7 +256,8 @@ def assign_one(
                             team_reviewers.add(team)
                     else:
                         ms = map_dev(memail, dev_mapping)
-                        if u := dev_mapping.get(memail.lower()) and u != pr_submitter:
+                        u = dev_mapping.get(memail.lower())
+                        if u not in (None, pr_submitter):
                             reviewers.add(u)
 
                     for subm in m:
